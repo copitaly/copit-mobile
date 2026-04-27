2c1c63d0377a86ba3ac5b11d8d3b289ea1248269
twinmindsolutions <twinmindsolutions@gmail.com>
2026-04-27 18:59:25 +0100

    display secondary path

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -72,6 +72,10 @@ export class HomePage implements OnInit, OnDestroy {
     void this.router.navigate(['/branches']);
   }
 
+  goToBranches(): void {
+    void this.router.navigate(['/branches']);
+  }
+
   goToAccount(isAuthenticated: boolean | null): void {
     void this.router.navigate([isAuthenticated ? '/profile' : '/login']);
   }
