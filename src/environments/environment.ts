0c38a19a836fcf085fac477b1717435edf8f287d
twinmindsolutions <twinmindsolutions@gmail.com>
2026-05-10 22:27:16 +0100

    moved server env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://cop-it-admin-staging-116a186a63a5.herokuapp.com/api',
+  apiBaseUrl: 'https://copit-api-staging.up.railway.app/api',
   appOrigin: 'http://localhost:8100',
   stripePublishableKey: 'pk_test_51THm0c5TkhnH7UiO7vLQPQiejjd6Fre5rY23sjYXZySj8t6WYsZDYsPkiO2kbhaktejllSv6XXdoRyS2sYxpDst700DkaOG4pC',
   stripeMerchantDisplayName: 'COP Italy Mobile',
