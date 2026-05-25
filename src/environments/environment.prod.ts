3b0828468e3cbf3fd9714721421a4dd7753e6552
twinmindsolutions <twinmindsolutions@gmail.com>
2026-05-25 17:46:25 +0100

    fixed api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://cop-it-admin-staging-116a186a63a5.herokuapp.com/api',
+  apiBaseUrl: 'https://copit-api-staging.up.railway.app/api',
   appOrigin: 'https://copit-staging.web.app',
   stripePublishableKey: 'pk_test_51THm0c5TkhnH7UiO7vLQPQiejjd6Fre5rY23sjYXZySj8t6WYsZDYsPkiO2kbhaktejllSv6XXdoRyS2sYxpDst700DkaOG4pC',
   stripeMerchantDisplayName: 'COP Italy',
