594172fd8922c99ca3ea9a4b6c3a9bd1f93ec274
twinmindsolutions <twinmindsolutions@gmail.com>
2026-05-30 11:10:32 +0100

    Rename package

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'copit-mobile',
+  appId: 'com.peniel.platform',
+  appName: 'COP Italy',
   webDir: 'www'
 };
 
